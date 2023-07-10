51b2e84a019293e863fd9c6ab45433e4ea309541
arex123 <ad47kumar@gmail.com>
2023-07-10 22:44:06 +0530

    form screen

---
+++ src/Components/Form.js
@@ -0,0 +1,21 @@
+import './Form.css'
+function Form() {
+    return <div className="form">
+        <div className="container">
+            <div className="title">
+                <h1>My account</h1>
+            </div>
+        </div>
+
+        <div className="form_container">
+            <div className="return_user">
+                <div></div>
+            </div>
+            <div className="new_user">
+                <div></div>
+            </div>
+        </div>
+    </div>
+
+}
+export default Form
